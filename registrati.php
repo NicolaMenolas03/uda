589b3d6ca50031da060ae5dcca859d0a04da478e
Nicola Menolascina <nicola.menola1505@gmail.com>
2022-04-09 17:36:55 +0200

    Modifiche registrati.php

--- registrati.php
+++ registrati.php
@@ -3,16 +3,15 @@
         Nome<input type="text" name="nome"><br>
         Cognome<input type="text" name="cognome"><br>
         Telefono<input type="text" name="telefono"><br>
-        Città<input type="text" name="telefono"><br>
+        Città<input type="text" name="citta"><br>
         Comune  <select id="comuni">
                     <?php
                     
-                        require_once('open_php.php');
+                        require_once('ConnessionDB.php');
 
                         $sql="SELECT IdComune,Comune FROM comuni"; 
                         $result=mysqli_query($conn,$sql);
-                        while($row=mysqli_fetch_array($result))
-                        {
+                        while($row=mysqli_fetch_array($result)){
                             echo "<option name='comune' value='".$row['IdComune']."'>".$row['Comune']."</option>";
                         } 
                         
@@ -30,7 +29,7 @@
         <option value="maestro" name="cartadicredito">Maestro</option>
         </select>-->
         Mail<input type="text" name="mail"><br>
-        Password<input type="password" name="password"><br>
+        Password<input type="password" name="password" autocomplete="on"><br>
        <input type="submit" name="invio">
     </form>
 
@@ -44,18 +43,49 @@
             }
         }
 
+        function controllonumero($numero){
+            if (is_numeric($_POST["telefono"])&&$_POST["telefono"]>3000000000){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        function controllousername($username){
+
+            $sql="SELECT UsernameCliente FROM clienti"; 
+            $result=mysqli_query($conn,$sql);
+            while($row=mysqli_fetch_array($result)){
+                if ($username!=$row["UsernameCliente"]){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            } 
+
+        }
+
         if (isset($_POST["invio"])){
-            if (!empty($_POST["nome"])&&!empty($_POST["cognome"])&&!empty($_POST["telefono"])&&!empty($_POST["username"]))    
-                if (!empty("mail")||!empty("password")){
-                    $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
+            if (!empty($_POST["nome"])&&!empty($_POST["cognome"])&&!empty($_POST["telefono"])&&!empty($_POST["citta"])&&!empty($_POST["comune"])&&!empty($_POST["via"])&&!empty($_POST["civico"])&&!empty($_POST["username"])){ 
+                if (!empty($_POST["mail"])||!empty($_POST["password"])){
+                    $mail = filter_var($_POST["mail"], FILTER_SANITIZE_EMAIL);
                     $controllo = controllamail($mail);
                     if ($controllo==true){
                         
-                        if (is_numeric($_POST["telefono"])&&$_POST["telefono"]>3000000000){
+                        $controllo = controllonumero($_POST["telefono"]);
+                        if ($controllo==true){
+                            
+                            $controllo = controllousername($_POST["username"]);
+                            if ($controllo==true){
+                                //reindirizzamento pagina per inserire dati carta di credito
+                            }
+                            else{
 
+                            }
                         }
                         else{
-
                         }
 
                     }
@@ -63,6 +93,10 @@
 
                     }
                 }
+            }
+            else{
+                echo "<h1>Riempire tutti i campi</h1>";
+            }
         }
     ?>
 </html>
